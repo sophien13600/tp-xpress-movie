7f0bcee17a218f073c0a35976bf8c57fd8654b37
Sophiene13 <bouderbala.s@gmail.com>
2025-09-21 11:01:33 +0200

    feat: authentification redirection via middleware si connecté

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,14 +1,15 @@
 import express from "express";
 import AuthController from "../controllers/auth.controller.js";
+import {redirectIfLoggedIn} from "../middlewares/auth.middleware.js"
 
 const router = express.Router();
 
-router.get("/inscription", (req, res) => {
+router.get("/inscription", redirectIfLoggedIn, (req, res) => {
     res.render("register");
   });
 router.post("/inscription", AuthController.addUser);
 
-router.get("/connexion", (req, res) => {
+router.get("/connexion", redirectIfLoggedIn, (req, res) => {
     res.render("login");
   });
 
